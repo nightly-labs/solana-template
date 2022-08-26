43d05e3f86058c0236dccbf1ca64db6418a8262e
adamkoot <adam.kot511@gmail.com>
2022-08-26 14:30:02 +0200

    final

--- src/App.tsx
+++ src/App.tsx
@@ -5,55 +5,44 @@ import {
   SystemProgram,
   Transaction as SolanaTx
 } from '@solana/web3.js'
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './App.css'
 import { NightlyWalletAdapter } from './nightly'
 import { NATIVE_MINT, TOKEN_PROGRAM_ID, Token } from '@solana/spl-token'
 import { Button, Typography } from '@material-ui/core'
-import { AppSolana } from '@nightlylabs/connect-solana'
-import { WebsocketBuilder } from 'websocket-ts'
-
-interface SolanaOnConnect {
-  publicKey: PublicKey
-}
-
-// interface SolanaAppInfo {
-//   application: string // 'Application name'
-//   description: string // 'Description'
-//   additionalInfo: string // Some Additional info
-//   icon: string // https://application/logo.png
-//   url?: string // default: wss://relay.nightly.app/app
-//   onUserConnect: (params: SolanaOnConnect) => void // userConnectedCallback
-//   timeout?: number //  40s default timeout (timer for Server answer request). On timeout throws Error 'Connection timed out'.
-// }
+import { NCSolanaWalletAdapter } from '@nightlylabs/connect-solana'
 
 const NightlySolana = new NightlyWalletAdapter()
 const connection = new Connection('https://api.devnet.solana.com')
 
-const APP_ADDRESS = 'wss://relay.nightly.app/app'
-
-function App() {
-  const testAppInfo = {
-    appMetadata: {
-      additionalInfo: ' Test Additional info',
-      application: 'Test application',
-      description: 'Test description',
-      icon: 'https://docs.nightly.app/img/logo.png',
-      persistent: false
-    },
-    url: APP_ADDRESS,
-    onUserConnect: ({ publicKey }) => {
-      setUserPublicKey(publicKey)
-    }
+const NightlyConnectSolana = new NCSolanaWalletAdapter({
+  appMetadata: {
+    persistent: false,
+    additionalInfo: ' Test Additional info',
+    application: 'Test application',
+    description: 'Test description',
+    icon: 'https://docs.nightly.app/img/logo.png'
   }
-
+})
+function App() {
   const [userPublicKey, setUserPublicKey] = useState<PublicKey | undefined>(undefined)
+  useEffect(() => {
+    NightlyConnectSolana.modal.onOpen = () => {
+      console.log('modal opened with event handler')
+    }
+    NightlyConnectSolana.on('connect', setUserPublicKey)
+    NightlyConnectSolana.on('error', error => {
+      console.log(error)
+    })
+  }, [])
+
   return (
     <div className='App'>
       <header className='App-header'>
         <Typography>
           {userPublicKey ? `Hello, ${userPublicKey.toBase58()}` : 'Hello, stranger'}
         </Typography>
+
         <Button
           variant='contained'
           color='primary'
@@ -73,62 +62,67 @@ function App() {
           color='primary'
           style={{ margin: 10 }}
           onClick={async () => {
-            //const a = new AppSolana(ws as WebSocket, 40)
-            try {
-              await AppSolana.build(testAppInfo)
-            } catch (er) {
-              console.log(er)
-            }
+            await NightlyConnectSolana.connect()
           }}>
           Nightly Connect
         </Button>
+
         <Button
           variant='contained'
           color='primary'
           style={{ margin: 10 }}
           onClick={async () => {
             if (!userPublicKey) return
+            try {
+              const wrappedSolAccount = Keypair.generate()
 
-            const wrappedSolAccount = Keypair.generate()
+              const createIx = SystemProgram.createAccount({
+                fromPubkey: userPublicKey,
+                newAccountPubkey: wrappedSolAccount.publicKey,
+                lamports: 10_000_000,
+                space: 165,
+                programId: TOKEN_PROGRAM_ID
+              })
 
-            const createIx = SystemProgram.createAccount({
-              fromPubkey: userPublicKey,
-              newAccountPubkey: wrappedSolAccount.publicKey,
-              lamports: 10_000_000,
-              space: 165,
-              programId: TOKEN_PROGRAM_ID
-            })
+              const initIx = Token.createInitAccountInstruction(
+                TOKEN_PROGRAM_ID,
+                NATIVE_MINT,
+                wrappedSolAccount.publicKey,
+                userPublicKey
+              )
 
-            const initIx = Token.createInitAccountInstruction(
-              TOKEN_PROGRAM_ID,
-              NATIVE_MINT,
-              wrappedSolAccount.publicKey,
-              userPublicKey
-            )
+              const approveIx = Token.createApproveInstruction(
+                TOKEN_PROGRAM_ID,
+                wrappedSolAccount.publicKey,
+                new PublicKey('147oKbjwGDHEthw7sRKNrzYiRiGqYksk1ravTMFkpAnv'),
+                userPublicKey,
+                [],
+                5000000
+              )
 
-            const approveIx = Token.createApproveInstruction(
-              TOKEN_PROGRAM_ID,
-              wrappedSolAccount.publicKey,
-              new PublicKey('147oKbjwGDHEthw7sRKNrzYiRiGqYksk1ravTMFkpAnv'),
-              userPublicKey,
-              [],
-              5000000
-            )
+              const tx = new SolanaTx()
 
-            const ix = SystemProgram.transfer({
-              fromPubkey: userPublicKey,
-              lamports: 1_000_000,
-              toPubkey: new PublicKey('147oKbjwGDHEthw7sRKNrzYiRiGqYksk1ravTMFkpAnv')
-            })
-            const tx = new SolanaTx()
-            tx.add(createIx).add(initIx).add(approveIx)
-            const a = await connection.getLatestBlockhash()
-            tx.recentBlockhash = a.blockhash
-            tx.feePayer = userPublicKey
-            const signedTx = await NightlySolana.signTransaction(tx)
-            signedTx.partialSign(wrappedSolAccount)
-            const id = await connection.sendRawTransaction(signedTx.serialize())
-            console.log(id)
+              tx.add(createIx).add(initIx).add(approveIx)
+
+              const a = await connection.getLatestBlockhash()
+
+              tx.recentBlockhash = a.blockhash
+              tx.feePayer = userPublicKey
+
+              if (NightlyConnectSolana.connected) {
+                const signedTx = await NightlyConnectSolana.signTransaction(tx)
+                signedTx.partialSign(wrappedSolAccount)
+                const id = await connection.sendRawTransaction(signedTx.serialize())
+                console.log(id)
+              } else {
+                const signedTx = await NightlySolana.signTransaction(tx)
+                signedTx.partialSign(wrappedSolAccount)
+                const id = await connection.sendRawTransaction(signedTx.serialize())
+                console.log(id)
+              }
+            } catch (err) {
+              console.log(err)
+            }
           }}>
           Approve 0.0005 SOL delegate and send 0.001 SOL
         </Button>
@@ -148,9 +142,15 @@ function App() {
             const a = await connection.getRecentBlockhash()
             tx.recentBlockhash = a.blockhash
             tx.feePayer = userPublicKey
-            const signedTx = await NightlySolana.signTransaction(tx)
-            const id = await connection.sendRawTransaction(signedTx.serialize())
-            console.log(id)
+            if (NightlyConnectSolana.connected) {
+              const signedTx = await NightlyConnectSolana.signTransaction(tx)
+              const id = await connection.sendRawTransaction(signedTx.serialize())
+              console.log(id)
+            } else {
+              const signedTx = await NightlySolana.signTransaction(tx)
+              const id = await connection.sendRawTransaction(signedTx.serialize())
+              console.log(id)
+            }
           }}>
           Send test 0.0005 SOL
         </Button>
@@ -200,7 +200,13 @@ function App() {
           color='secondary'
           style={{ margin: 10 }}
           onClick={async () => {
-            await NightlySolana.disconnect()
+            try {
+              await NightlySolana.disconnect()
+              await NightlyConnectSolana.disconnect()
+              setUserPublicKey(undefined)
+            } catch (err) {
+              console.log(err)
+            }
           }}>
           Disconnect Solana
         </Button>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: ['ES2020']
+  },
   plugins: [
     react()
     // nodePolyfills(),
