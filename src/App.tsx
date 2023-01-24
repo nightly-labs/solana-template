1c82565738fce223ea76352df77e735de13a929b
awojciak <adrianwojciak99@gmail.com>
2023-01-24 15:05:00 +0100

    simple address

--- src/App.tsx
+++ src/App.tsx
@@ -268,9 +268,7 @@ function App() {
           color='secondary'
           style={{ margin: 10, backgroundColor: 'gold', color: 'black' }}
           onClick={async () => {
-            const address = !!`${process.env.VITE_VERCEL_URL}`.length
-              ? `${process.env.VITE_VERCEL_URL}`
-              : 'https://solana-template-ten.vercel.app/'
+            const address = 'https://solana-template-ten.vercel.app/'
             const a = await connection.getRecentBlockhash()
             const txs = [
               Buffer.from(
